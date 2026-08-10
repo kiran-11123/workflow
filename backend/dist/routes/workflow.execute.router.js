5bb732df0c953d5d3f5db00832be5169262f34a9
Kiran <kiran.sadara6730@gmail.com>
2026-08-10 22:04:20 +0530

    adding the users-backend

--- backend/dist/routes/workflow.execute.router.js
+++ backend/dist/routes/workflow.execute.router.js
@@ -2,6 +2,6 @@ import express from 'express';
 const execute_router = express.Router();
 import { WorkFlowExecutorController } from '../controllers/workflow.execute.controller.js';
 const controller = new WorkFlowExecutorController();
-execute_router.post('/:id', controller.execute);
+execute_router.post('/:id', controller.execute.bind(controller));
 export default execute_router;
 //# sourceMappingURL=workflow.execute.router.js.map
--- backend/src/routes/workflow.execute.router.ts
+++ backend/src/routes/workflow.execute.router.ts
@@ -2,7 +2,7 @@ import express from 'express'
 const execute_router = express.Router();
 import { WorkFlowExecutorController } from '../controllers/workflow.execute.controller.js';
 const controller = new WorkFlowExecutorController();
-execute_router.post('/:id' , controller.execute)
+execute_router.post('/:id' , controller.execute.bind(controller))
 
 
 
